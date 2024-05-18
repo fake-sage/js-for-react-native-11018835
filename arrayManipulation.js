28e206fecbed17becd2f53b35ff92329c14f4cd9
Nii Ohene Omane-Addo <151413334+fake-sage@users.noreply.github.com>
2024-05-18 22:38:48 +0000

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,6 +9,18 @@ function processArray(arr) {
 }
 
 
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+
+const strings = ["Hello", "World", "Assignment 1", "11018835", "Commit 2"];
 const numbers = [1, 2, 3, 4, 5];
 const processedNumbers = processArray(numbers);
-console.log(processedNumbers); 
+const formattedStrings = formatArrayStrings(strings, processedNumbers);
+console.log(formattedStrings); 
